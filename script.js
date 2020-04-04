74fd2a6d80f3e5e151a985a75f5bde2195edd9e5
Marta Bednarz <martab2525@gmail.com>
2020-04-04 17:55:30 +0200

    add calculations

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const myBmi = document.getElementById('btn');
+
+
+myBmi.addEventListener('click', function () {
+  const height = document.getElementById('height').value;
+  const weight = document.getElementById('weight').value;
+  const bmi = weight / (height * 0.01)**2;
+  const newBmi = bmi.toFixed(2);
+  document.getElementById('result').innerHTML = "Your BMI is " + newBmi;
+})
+
